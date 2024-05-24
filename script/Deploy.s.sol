bf5e9532aae54e5293b0e25fd85e09911798f986
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2024-05-24 08:23:50 -0400

    test

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,9 +4,27 @@ pragma solidity >=0.8.0;
 import "utils/BaseScript.sol";
 
 contract DeployScript is BaseScript {
+    bytes32 constant SALT = bytes32(keccak256("1716400568_0"));
+
     function deploy() public {
+        /*
+        
+    function deployUsingCreate3(
+        string memory deploymentName,
+        bytes32 salt,
+        string memory artifact,
+        bytes memory args,
+        uint value
+    ) internal returns (address deployed) {
+        */
+
+        uint256 totalSupply = 1_946_000 ether;
+        address safe = toolkit.getAddress(ChainId.All, "safe");
+        address feeCollector = safe;
+
+       bytes memory args = abi.encode("boomer", "$boomer", feeCollector, 100, 0, address(0), 12 * 10**15);
         vm.startBroadcast();
-        deploy("MyContract", "MyContract.sol:MyContract", abi.encode(tx.origin));
+        deployUsingCreate3("Ticker", SALT, "OFT20Ticker.sol:OFT20Ticker", abi.encode(args), 0);
         vm.stopBroadcast();
     }
 }
